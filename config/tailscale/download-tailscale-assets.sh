e3f78eed841fcbc1d6492a2bc4d9234e74d9449e
Philip Crockett <contact@philcrockett.com>
2021-12-27 20:49:40 +0100

    Tailscale

---
+++ config/tailscale/download-tailscale-assets.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# This script exists mostly just to document where the Tailscale stuff
+# came from.
+
+# https://tailscale.com/download/linux/ubuntu-2110
+
+set -Eeuo pipefail
+
+script_dir=$(dirname "$(readlink -f "${0}")")
+key_path="${script_dir}/tailscale.gpg"
+list_path="${script_dir}/tailscale.list"
+
+curl --fail --silent --show-error \
+    --location https://pkgs.tailscale.com/stable/ubuntu/impish.gpg \
+    | gpg --dearmor > "${key_path}"
+
+curl --fail --silent --show-error \
+    --location https://pkgs.tailscale.com/stable/ubuntu/impish.list \
+    > "${list_path}"
+
+echo "Success. See files:
+  * ${key_path}
+  * ${list_path}
+"
+
